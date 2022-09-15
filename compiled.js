c44e0bc934d0a019cdd550e3374b494624c675e3
wxy <2586958565@qq.com>
2022-09-15 21:10:44 +0800

    添加案例

--- compiled.js
+++ compiled.js
@@ -14,19 +14,7 @@ var ast = _core["default"].parseSync(content, {
   sourceType: 'module'
 });
 
-var dependencies = []; // 使用 babel 提供的 traverse 方法遍历 AST并分析当前模块依赖的模块
-// 具体方法是：我们检查 AST 中每个 import 声明语句
-
-_core["default"].traverse(ast, {
-  // 由于 ESM 是静态的，所以要分析他很容易
-  // “静态”意味着你不能引入一个变量，也不能根据条件引入其他模块
-  // 每当我们看到一个 import 声明，就将他的值记录下来作为依赖
-  ImportDeclaration: function ImportDeclaration(_ref) {
-    var node = _ref.node;
-    // 将 import 的值存入 dependencies 数组中
-    dependencies.push(node.source.value);
-  }
-});
+_core["default"].traverse(ast);
 
 var _babel$transformFromA = _core["default"].transformFromAstSync(ast, null, {
   presets: ['@babel/preset-env']
---
+++ compiled/demo1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var a = function a() {
+  console.log('hello');
+};
+
+var arr = [1, 2, 3, 6, 4, 3];
+arr.toReversed();
+console.log(arr);
+console.log(arr);
