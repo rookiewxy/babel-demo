acb17a45690d86251913b67fca9cdeba63f54ed6
wxy <2586958565@qq.com>
2022-09-16 16:42:21 +0800

    babel编译

---
+++ 1.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Autor: wxy
+ * @Date: 2022-09-16 16:40:07
+ * @LastEditors: wxy
+ * @LastEditTime: 2022-09-16 16:40:22
+ */
+function aaa(){
+  return 'hello'
+}
+aaa()
--- demo.js
+++ demo.js
@@ -1,12 +1,13 @@
-import babel from '@babel/core';
-import fs from 'fs'
-// 将文件内容读取为字符串
+const babel = require('@babel/core')
+const fs = require('fs')
+
 const content = fs.readFileSync('./1.js', 'utf-8');
-// 在这里我们指明：我们的代码采用 ESM(EcmaScript modules) 模块系统，babel 会基于 ESM 的语法寻找文件的依赖关系
+
 const ast = babel.parseSync(content, {
   sourceType: 'module',
 });
 babel.traverse(ast);
+console.log(ast);
 
 const {code} = babel.transformFromAstSync(ast, null, {
   presets: ['@babel/preset-env'],
